4af6a946750f9e7778da27048a9a5436cb83d729
Journeyman <antisocial@storiepvtride.it>
2017-10-07 01:56:48 +0200

    remove unreachable code, improve error msg

--- nth-prime/src/lib.rs
+++ nth-prime/src/lib.rs
@@ -1,5 +1,7 @@
 #[derive(Debug, PartialEq)]
-pub struct Error;
+pub struct Error<T> {
+    msg: T,
+}
 
 fn is_prime(n: i32) -> bool {
     if n < 2 {
@@ -13,20 +15,20 @@ fn is_prime(n: i32) -> bool {
     return true;
 }
 
-pub fn nth(n: i32) -> Result<i32, Error> {
+pub fn nth(n: i32) -> Result<i32, Error<String>> {
     match n {
-        0 => Err(Error),
+        0 => Err(Error { msg: format!("zeroth prime") }),
         _ => {
             let mut count = 0;
             for x in 0.. {
-                if is_prime(x) == true {
+                if is_prime(x) {
                     count += 1;
                 }
                 if count == n {
                     return Ok(x);
                 }
             }
-            Ok(count)
+            Err(Error { msg: format!("did not find any prime for {}", n) })
         }
     }
 }
