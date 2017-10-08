f2444c4b085fef13f4bfdf0a7305aff2820cecaa
Journeyman <antisocial@storiepvtride.it>
2017-10-08 14:22:59 +0200

    add exercism.nth-prime new algo

--- nth-prime/src/lib.rs
+++ nth-prime/src/lib.rs
@@ -3,7 +3,8 @@ pub struct Error<T> {
     msg: T,
 }
 
-fn is_prime(n: i32) -> bool {
+// brute force
+fn __is_prime(n: i32) -> bool {
     if n < 2 {
         return false;
     }
@@ -15,12 +16,38 @@ fn is_prime(n: i32) -> bool {
     return true;
 }
 
+// AKS (?)
+fn is_prime(n: i32) -> bool {
+    if n < 2 {
+        return false;
+    }
+    if (n == 2) || (n == 3) {
+        return true;
+    }
+    if (n % 2 == 0) || (n % 3 == 0) {
+        return false;
+    }
+
+    let mut i = 5;
+    let mut w = 2;
+    while i < n {
+        if n % i == 0 {
+            return false;
+        }
+        i += w;
+        w = 6 - w; // either 4 or 2
+    }
+
+
+    return true;
+}
+
 pub fn nth(n: i32) -> Result<i32, Error<String>> {
     match n {
         0 => Err(Error { msg: format!("zeroth prime") }),
         _ => {
             let mut count = 0;
-            for x in 0.. {
+            for x in 1.. {
                 if is_prime(x) {
                     count += 1;
                 }
